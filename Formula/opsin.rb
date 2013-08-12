a24e4afdfb837785a1fec5b0a47414af184d6fde
Matt Swain <m.swain@me.com>
2013-08-12 12:16:50 +0100

    opsin 1.5.0: New formula

---
+++ Formula/opsin.rb
@@ -0,0 +1,30 @@
+require 'formula'
+
+class Opsin < Formula
+  homepage 'https://bitbucket.org/dan2097/opsin'
+  url 'https://bitbucket.org/dan2097/opsin/downloads/opsin-1.5.0-jar-with-dependencies.jar'
+  sha1 '4b6d779498ee629b3a01b22a68bf1a29b437658a'
+
+  def install
+    libexec.install "opsin-1.5.0-jar-with-dependencies.jar"
+    (bin+"opsin").write <<-EOS.undent
+      #!/bin/sh
+      java -jar "#{libexec}/opsin-1.5.0-jar-with-dependencies.jar" "$@"
+    EOS
+  end
+
+  def caveats; <<-EOS.undent
+    The OPSIN jar file has been installed to:
+      #{libexec}/opsin-1.5.0-jar-with-dependencies.jar
+    This may need to be added to the java CLASSPATH environment variable.
+    OPSIN can be run from the command line using the `opsin` tool. For help use:
+      opsin -h
+    EOS
+  end
+
+  test do
+    system "opsin -h"
+    system "echo '2,4,6-trinitrotoluene' | opsin"
+    system "java -jar '#{libexec}/opsin-1.5.0-jar-with-dependencies.jar' -h"
+  end
+end
