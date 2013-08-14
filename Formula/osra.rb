392749308d8f34421c1c234b9683935af2ed267a
Matt Swain <m.swain@me.com>
2013-08-14 10:54:06 +0100

    osra 2.0.0: New formula

---
+++ Formula/osra.rb
@@ -0,0 +1,31 @@
+require 'formula'
+
+class Osra < Formula
+  homepage ''
+  url 'http://sourceforge.net/projects/osra/files/osra/2.0.0/osra-2.0.0.tgz'
+  sha1 '318862b556014066d68342a843a470993877e8a9'
+
+  depends_on 'freetype'
+  depends_on 'libpng'
+  depends_on 'libtiff'
+  depends_on 'lcms'
+  depends_on 'jasper'
+  depends_on 'potrace'
+  depends_on 'jpeg'
+  depends_on 'ocrad'
+  depends_on 'tclap'
+  depends_on 'mcs07/cheminformatics/gocr' => 'with-libs'
+  depends_on 'mcs07/cheminformatics/open-babel'
+  depends_on 'graphicsmagick'
+  depends_on 'ghostscript' => :optional
+
+  def install
+    system "./configure", "--prefix=#{prefix}"
+    system "make"
+    system "make", "install"
+  end
+
+  test do
+    system "#{bin}/osra --help"
+  end
+end
