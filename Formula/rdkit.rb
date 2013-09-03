af51f05b2f881b87abc8fa8175920c67ce8d186f
Matt Swain <m.swain@me.com>
2013-09-03 16:07:51 +0100

    rdkit 2013.06.1: PostgreSQL cartridge option
    Added '--with-postgresql' option to build the RDKit PostgreSQL database cartridge.

--- Formula/rdkit.rb
+++ Formula/rdkit.rb
@@ -2,26 +2,27 @@ require 'formula'
 
 class Rdkit < Formula
   homepage 'http://rdkit.org/'
-  url 'http://sourceforge.net/projects/rdkit/files/rdkit/Q2_2013/RDKit_2013_06_1.tgz'
+  url 'http://downloads.sourceforge.net/projects/rdkit/files/rdkit/Q2_2013/RDKit_2013_06_1.tgz'
   sha1 '4c69afe1a93bd7529db65597a70515cf28dde6a3'
   head 'https://github.com/rdkit/rdkit.git'
-  
+
   option 'without-python', 'Build without Python language bindings'
   option 'without-inchi', 'Build without InChI support'
+  option 'with-postgresql', 'Build with PostgreSQL database cartridge'
 
   depends_on 'cmake' => :build
   depends_on 'inchi' => :recommended
   depends_on :python => :recommended
+  depends_on :postgresql => :optional
   depends_on 'numpy' => :python if build.with? 'python'
   depends_on 'boost'
-  depends_on 'swig'
-  
+  depends_on 'swig' => :build
+
   def install
     args = std_cmake_parameters.split
     args << '-DRDK_INSTALL_INTREE=OFF'
-    args << '-DRDK_INSTALL_STATIC_LIBS=OFF'
     if build.with? 'inchi'
-      args << '-DRDK_BUILD_INCHI_SUPPORT=ON' 
+      args << '-DRDK_BUILD_INCHI_SUPPORT=ON'
       args << "-DINCHI_INCLUDE_DIR=#{HOMEBREW_PREFIX}/include/inchi/"
       args << "-DINCHI_LIBRARIES='#{HOMEBREW_PREFIX}/lib/libinchi.dylib'"
     end
@@ -37,5 +38,13 @@ class Rdkit < Formula
     system "make"
     system "make install"
     rm_f Dir["#{lib}/*.cmake"]
+    if build.include? 'with-postgresql'
+      ENV['RDBASE'] = "#{prefix}"
+      ENV.append 'CFLAGS', "-I#{include}/rdkit"
+      cd 'Code/PgSQL/rdkit' do
+        system "make"
+        system "make install"
+      end
+    end
   end
 end
