2e24dd2d6213f715d290ecbd90e54fc13c176673
Matt Swain <m.swain@me.com>
2014-03-18 16:37:39 +0000

    inchi: Add download mirror because InChI Trust website is down

--- Formula/inchi.rb
+++ Formula/inchi.rb
@@ -3,12 +3,13 @@ require 'formula'
 class Inchi < Formula
   homepage 'http://www.inchi-trust.org/downloads/'
   url 'http://www.inchi-trust.org/fileadmin/user_upload/software/inchi-v1.04/INCHI-1-API.ZIP'
+  mirror 'http://assets.matt-swain.com/inchi/INCHI-1-API.ZIP'
   version '1.04'
   sha1 '46a99a532ae6fcec40efe20abafed0ed52d73c43'
 
   def patches
     # Patch makefile to support Mac OS X
-    'https://gist.github.com/mcs07/6194763/raw'
+    'https://gist.github.com/mcs07/6194763/raw/2edc62ed259fa8970a9c9bbd9b937afc2cf45f98/inchi-osx.diff'
   end
 
   def install
