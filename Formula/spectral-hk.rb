700b5beafb975e4386de9151c0443b84a2e6f6aa
Matt Swain <m.swain@me.com>
2014-04-05 13:06:13 +0100

    spectral-hk: New formula

---
+++ Formula/spectral-hk.rb
@@ -0,0 +1,26 @@
+require 'formula'
+
+class SpectralHk < Formula
+  homepage 'https://bitbucket.org/ncgc/spectral_hk/overview'
+  head do
+    url 'https://bitbucket.org/ncgc/spectral_hk.git'
+    depends_on 'gsl' => :recommended
+  end
+
+  def install
+    if build.with? 'gsl'
+      system 'make', "-f", "Makefile.gsl",
+                     "SUFFIX=",
+                     "GSLFLAGS=-DHAVE_GSL -I#{HOMEBREW_PREFIX}/include",
+                     "GSLLIBS=-L#{HOMEBREW_PREFIX}/lib -lgsl -lgslcblas"
+    else
+      system "make"
+    end
+    bin.install "spectral_hk"
+    prefix.install "examples.txt"
+  end
+
+  test do
+    system "#{bin}/spectral_hk", "#{prefix}/examples.txt"
+  end
+end
