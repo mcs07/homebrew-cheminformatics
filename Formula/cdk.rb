022023dc6bcd1a4a5d25e2d88b7f50044495a42a
Matt Swain <m.swain@me.com>
2014-05-06 17:15:34 +0100

    cdk: Fix download link 404

--- Formula/cdk.rb
+++ Formula/cdk.rb
@@ -2,7 +2,7 @@ require 'formula'
 
 class Cdk < Formula
   homepage 'http://cdk.sf.net/'
-  url 'http://downloads.sourceforge.net/projects/cdk/files/cdk/1.4.19/cdk-1.4.19.jar'
+  url 'http://downloads.sourceforge.net/project/cdk/cdk/1.4.19/cdk-1.4.19.jar'
   sha1 '4f31a6a0a5eba376736eb45b3e5cad4a895aa786'
 
   def install
