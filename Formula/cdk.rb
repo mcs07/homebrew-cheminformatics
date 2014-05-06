b5192821b77022a2a95757f867239ddfeefb87a6
Matt Swain <m.swain@me.com>
2014-05-06 17:33:42 +0100

    cdk: Add HEAD option

--- Formula/cdk.rb
+++ Formula/cdk.rb
@@ -5,9 +5,22 @@ class Cdk < Formula
   url 'http://downloads.sourceforge.net/project/cdk/cdk/1.4.19/cdk-1.4.19.jar'
   sha1 '4f31a6a0a5eba376736eb45b3e5cad4a895aa786'
 
+  head do
+    url 'https://github.com/cdk/cdk.git'
+    depends_on 'maven'
+  end
+
   def install
-    mv 'cdk-1.4.19.jar', 'cdk.jar'
-    libexec.install 'cdk.jar'
+    if build.head?
+      cd 'bundle' do
+        system 'mvn', 'package'
+        mv Dir['target/cdk-[1-9].[0-9].[0-9]-SNAPSHOT.jar'].to_s, 'target/cdk.jar'
+        libexec.install 'target/cdk.jar'
+      end
+    else
+      mv 'cdk-1.4.19.jar', 'cdk.jar'
+      libexec.install 'cdk.jar'
+    end
   end
 
   def caveats; <<-EOS.undent
