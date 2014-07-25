0be0bcade824a0f62041b44ec8305bdf5563e2ad
Matt Swain <m.swain@me.com>
2014-07-25 15:41:42 +0100

    osra: Fix download URL

--- Formula/osra.rb
+++ Formula/osra.rb
@@ -2,13 +2,13 @@ require 'formula'
 
 class Osra < Formula
   homepage 'http://osra.sourceforge.net'
-  url 'http://sourceforge.net/projects/osra/files/osra/2.0.0/osra-2.0.0.tgz'
+  url 'https://downloads.sourceforge.net/project/osra/osra/2.0.0/osra-2.0.0.tgz'
   sha1 '318862b556014066d68342a843a470993877e8a9'
 
   depends_on 'freetype'
   depends_on 'libpng'
   depends_on 'libtiff'
-  depends_on 'lcms'
+  depends_on 'little-cms'
   depends_on 'jasper'
   depends_on 'potrace'
   depends_on 'jpeg'
