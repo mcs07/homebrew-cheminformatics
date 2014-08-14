683a545e86b11633146afb706ae8a44357e9df98
Matt Swain <m.swain@me.com>
2014-08-14 15:17:52 +0100

    cluster: Add bottle

--- Formula/cluster.rb
+++ Formula/cluster.rb
@@ -5,13 +5,19 @@ class Cluster < Formula
   url "http://bonsai.hgc.jp/~mdehoon/software/cluster/cluster-1.52.tar.gz"
   sha1 "7e983ffc8951a183e717c30f3b25b655c2fd697f"
 
+  bottle do
+    root_url "http://assets.matt-swain.com/homebrew"
+    cellar :any
+    sha1 "60270744d3c61ea7b625190efdb05742aeebcb55" => :mavericks
+  end
+
   option 'with-lesstif',  'Build GUI'
 
   depends_on "lesstif" => :optional
 
   def install
     args = "--disable-debug", "--disable-dependency-tracking", "--disable-silent-rules", "--prefix=#{prefix}"
-    args << "--without-x" if not build.with? "lesstif"
+    args << "--without-x" if build.without? "lesstif"
     system "./configure", *args
     system "make", "install"
   end
