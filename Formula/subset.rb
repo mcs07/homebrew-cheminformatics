34c6552150c2f5e8bca01c023e3ee47f6fe958bb
Matt Swain <m.swain@me.com>
2014-08-14 13:23:26 +0100

    subset: New formula

---
+++ Formula/subset.rb
@@ -0,0 +1,26 @@
+require "formula"
+
+class Subset < Formula
+  homepage "http://cactus.nci.nih.gov/subset/"
+  url "http://cactus.nci.nih.gov/subset/subset_1.0.tgz"
+  sha1 "0d6910eb029bd97488d11132acad0ca7567ce9a4"
+
+  # Patch to skip tests in makefile
+  patch do
+    url 'https://gist.githubusercontent.com/mcs07/348832e0a6640aa21758/raw/4f8698c103b1ba2b5e01707810f1e187c0495163/subset.diff'
+    sha1 '405a89c9929d00de434c4133f9f2798c72465cc0'
+  end
+
+  def install
+    system "make"
+    bin.install "subset"
+  end
+
+  test do
+    File.open("label_vector.tab", "w") do |f|
+      f.write('vector1   1010101010101010101101010101\nvector2   1010101010101010101101010101')
+    end
+    system "subset -sim 0.5 < label_vector.tab"
+    system "subset -sim 0.5 -log test.log < label_vector.tab"
+  end
+end
