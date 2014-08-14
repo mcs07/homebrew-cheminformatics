64893f5b257ef671b92f069a7ad03afbdbd20975
Matt Swain <m.swain@me.com>
2014-08-14 15:26:31 +0100

    critic2: Add bottle

--- Formula/critic2.rb
+++ Formula/critic2.rb
@@ -5,6 +5,11 @@ class Critic2 < Formula
   url "http://gatsby.ucmerced.edu/downloads/critic2/critic2-1.0.tar.gz"
   sha1 "2b824a23def56e814d0893b89b26881f1373fb32"
 
+  bottle do
+    root_url "http://assets.matt-swain.com/homebrew"
+    sha1 "df36477c66f1872fedd786a3e3ad046fd7ea1edf" => :mavericks
+  end
+
   option "with-libxc", "Build with libxc support to calculate exchange and correlation energy densities"
 
   depends_on :fortran
