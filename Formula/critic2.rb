8b15976c6a47a6d1dcafc5288e87e635e27d3998
Matt Swain <m.swain@me.com>
2014-11-12 16:32:24 +0000

    critic2: Update to v3c65b7f
    critic2 seems to use git commit hashes instead of version numbers now. Hopefully this won't cause any issues.

--- Formula/critic2.rb
+++ Formula/critic2.rb
@@ -2,14 +2,13 @@ require "formula"
 
 class Critic2 < Formula
   homepage "http://gatsby.ucmerced.edu/wiki/Critic2"
-  url "http://gatsby.ucmerced.edu/downloads/critic2/critic2-1.0.tar.gz"
-  sha1 "e0899ad64e10e63226ae69433af717535b5bca7f"
+  url "http://gatsby.ucmerced.edu/downloads/critic2/critic2.tar.gz"
+  sha1 "ef56d588e770b6f783ab731b150b88cbcd37f60d"
+  version "3c65b7f"
 
   bottle do
     root_url "http://assets.matt-swain.com/homebrew"
-    revision 1
-    sha1 "0af1bfae11e549f90e150c8f4949708accde0f7b" => :mavericks
-    sha1 "81d95920aa175c5de1394a234b73a5479470e33f" => :yosemite
+    sha1 "7c3d6e4002b2dfa974a8953365812be28b93693a" => :yosemite
   end
 
   option "with-libxc", "Build with libxc support to calculate exchange and correlation energy densities"
