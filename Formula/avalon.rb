2df21768c039ce101ccf8db54182788e3efb28b6
Matt Swain <m.swain@me.com>
2014-11-13 14:43:51 +0000

    avalon: Update to v1.2.0

--- Formula/avalon.rb
+++ Formula/avalon.rb
@@ -1,28 +1,28 @@
 require 'formula'
 
 class Avalon < Formula
-  homepage 'http://sourceforge.net/projects/avalontoolkit/'
-  url 'https://downloads.sourceforge.net/project/avalontoolkit/AvalonToolkit_1.1_beta/AvalonToolkit_1.1_beta.source.tar'
-  version '1.1b'
-  sha1 'a0b15075c015884bc0c0a92c53b47368f45274fe'
+  homepage "http://sourceforge.net/projects/avalontoolkit/"
+  url "https://downloads.sourceforge.net/project/avalontoolkit/AvalonToolkit_1.2/AvalonToolkit_1.2.0.source.tar"
+  version "1.2.0"
+  sha1 "d2936b1b55634d5aeffa2ba9514ab9d8ce50023f"
 
   bottle do
     root_url "http://assets.matt-swain.com/homebrew"
     cellar :any
-    sha1 "bd4d459e6a70d3bfb0d07925f477a64715091dc9" => :mavericks
-    sha1 "b95bab21cdeb7dddbfbba196a59caf75b32a0e49" => :yosemite
+    sha1 "cfafa32fb50f11f6b22e007a763fd60154e2200d" => :yosemite
   end
 
   def install
-    cd 'SourceDistribution' do
-      inreplace 'makefile', 'platform.makefile', 'linux64.makefile'
-      system 'make', 'common', 'CC=cc -fPIC -fno-exceptions -Wno-return-type ', 'LD=cc', 'BITS=64',
-                     'LD_OPTS= -fPIC -lm -fno-exceptions ', 'COMMONDIR=../SourceDistribution/common'
-      bin.install 'canonizer', 'mol2smi', 'mol2tbl', 'smi2mol', 'struchk'
+    cd "SourceDistribution" do
+      inreplace "makefile", "platform.makefile", "non_windows.makefile"
+      system "make", "common", "CC=cc -fPIC -fno-exceptions -Wno-return-type ", "LD=cc", "BITS=64",
+                     "LD_OPTS= -fPIC -lm -fno-exceptions ", "COMMONDIR=../SourceDistribution/common"
+      bin.install "canonizer", "matchtest", "mol2fp", "mol2smi", "mol2tbl", "smi2fp", "smi2mol", "struchk"
     end
   end
 
   test do
-    system "#{bin}/smi2mol c1ccccc1 benzene.mol"
+    system "smi2mol c1ccccc1 benzene.mol"
+    system "mol2smi benzene.mol"
   end
 end
