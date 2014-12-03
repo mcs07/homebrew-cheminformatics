1430010879453981c5d11c0a483f37e677f32c94
Matt Swain <m.swain@me.com>
2014-12-03 15:51:50 +0000

    critic2: Switch to HEAD only

--- Formula/critic2.rb
+++ Formula/critic2.rb
@@ -2,13 +2,9 @@ require "formula"
 
 class Critic2 < Formula
   homepage "http://gatsby.ucmerced.edu/wiki/Critic2"
-  url "http://gatsby.ucmerced.edu/downloads/critic2/critic2.tar.gz"
-  sha1 "0756b84c2a904271522208a254e6b8c135ad6b0f"
-  version "443f7fb"
 
-  bottle do
-    root_url "http://assets.matt-swain.com/homebrew"
-    sha1 "5286279be83970c78c3655da85b2984a5f87230f" => :yosemite
+  head do
+    url "http://gatsby.ucmerced.edu/downloads/critic2/critic2.tar.gz"
   end
 
   option "with-libxc", "Build with libxc support to calculate exchange and correlation energy densities"
