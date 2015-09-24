bece680cd30869b121f40fed20bf53da55746cef
Matt Swain <m.swain@me.com>
2015-09-24 23:19:39 +0100

    filter-it: Use sha256

--- Formula/filter-it.rb
+++ Formula/filter-it.rb
@@ -4,7 +4,7 @@ class FilterIt < Formula
   homepage 'http://silicos-it.be.s3-website-eu-west-1.amazonaws.com/software/filter-it/1.0.2/filter-it.html'
   url 'http://silicos-it.be.s3-website-eu-west-1.amazonaws.com/_downloads/filter-it-1.0.2.tar.gz'
   mirror 'http://assets.matt-swain.com/homebrew/filter-it-1.0.2.tar.gz'
-  sha1 '25dbb320c5e7f17cb6755fe49791b16a36ac604c'
+  sha256 '386a94f092a1721351afac22f419fa5944c385a1908d6e6ec8b901764913efe0'
 
   bottle do
     root_url "http://assets.matt-swain.com/homebrew"
