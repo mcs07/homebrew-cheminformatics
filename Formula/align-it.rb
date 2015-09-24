5a045cfd6f650a1e390cdb9eb0996e7497d3d65c
Matt Swain <m.swain@me.com>
2015-09-24 23:06:25 +0100

    align-it: Use sha256

--- Formula/align-it.rb
+++ Formula/align-it.rb
@@ -4,7 +4,7 @@ class AlignIt < Formula
   homepage 'http://silicos-it.be.s3-website-eu-west-1.amazonaws.com/software/align-it/1.0.4/align-it.html'
   url 'http://silicos-it.be.s3-website-eu-west-1.amazonaws.com/_downloads/align-it-1.0.4.tar.gz'
   mirror 'http://assets.matt-swain.com/homebrew/align-it-1.0.4.tar.gz'
-  sha1 '3486b5bbdb970cd73ed332a89ba1807afe393e92'
+  sha256 '63601c67e0d95f5d7c0afdba9e4e74b9d36d7850b0204c8bba096a4257afd7e9'
 
   bottle do
     root_url "http://assets.matt-swain.com/homebrew"
