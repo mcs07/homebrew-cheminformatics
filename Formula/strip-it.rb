fb5d3e532b788f28d4616d2500770591a89e8cc0
Matt Swain <m.swain@me.com>
2015-09-24 23:38:35 +0100

    strip-it: Use sha256

--- Formula/strip-it.rb
+++ Formula/strip-it.rb
@@ -4,7 +4,7 @@ class StripIt < Formula
   homepage 'http://silicos-it.be.s3-website-eu-west-1.amazonaws.com/software/strip-it/1.0.2/strip-it.html'
   url 'http://silicos-it.be.s3-website-eu-west-1.amazonaws.com/_downloads/strip-it-1.0.2.tar.gz'
   mirror 'http://assets.matt-swain.com/homebrew/strip-it-1.0.2.tar.gz'
-  sha1 '5bd77184602475ce336d804077be0f7bb28bd6b0'
+  sha256 '02525444e75ceb6fcb9f218e53d6ad76a3f829a96913a86040fd180ded880131'
 
   bottle do
     root_url "http://assets.matt-swain.com/homebrew"
