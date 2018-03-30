b3fdb06e735322d52a883b6c3f8847765db15733
Matt Swain <m.swain@me.com>
2018-03-30 18:07:32 +0100

    open-babel: Fix for homebrew python handling

--- Formula/open-babel.rb
+++ Formula/open-babel.rb
@@ -8,29 +8,31 @@ class OpenBabel < Formula
 
   option 'without-cairo', 'Build without PNG depiction'
   option 'with-java', 'Build with Java language bindings'
-  option 'with-python', 'Build with Python language bindings'
-  option 'with-python3', 'Build with Python 3 language bindings'
+  option 'with-python@2', 'Build with Python 2 language bindings'
+  option 'with-python', 'Build with Python 3 language bindings'
   option 'with-wxmac', 'Build with GUI'
+
+  deprecated_option 'with-python3' => 'with-python'
 
   depends_on 'pkg-config' => :build
   depends_on 'cmake' => :build
-  depends_on :python => :optional
-  depends_on :python3 => :optional
+  depends_on 'python@2' => :optional
+  depends_on 'python' => :optional
   depends_on 'wxmac' => :optional
   depends_on 'cairo' => :recommended
-  depends_on 'swig' if build.with?('python') || build.with?('python3') || build.with?('java')
+  depends_on 'swig' => :build if build.with?('python@2') || build.with?('python') || build.with?('java')
   depends_on 'eigen'
   depends_on 'mcs07/cheminformatics/inchi'
 
   def install
     args = std_cmake_args
     args << "-DOPENBABEL_USE_SYSTEM_INCHI=ON"
-    args << "-DRUN_SWIG=ON" if build.with?('python') || build.with?('python3') || build.with?('java')
+    args << "-DRUN_SWIG=ON" if build.with?('python@2') || build.with?('python') || build.with?('java')
     args << "-DJAVA_BINDINGS=ON" if build.with? 'java'
     args << "-DBUILD_GUI=ON" if build.with? 'wxmac'
 
-    if build.with?('python') || build.with?('python3')
-      pyexec = if build.with?('python3') then `which python3`.strip else `which python`.strip end
+    if build.with?('python@2') || build.with?('python')
+      pyexec = if build.with?('python') then `which python3`.strip else `which python`.strip end
       pyvers = 'python' + %x(#{pyexec} -c 'import sys;print(sys.version[:3])').strip
       pypref = %x(#{pyexec} -c 'import sys;print(sys.prefix)').strip
       pyinc = %x(#{pyexec} -c 'from distutils import sysconfig;print(sysconfig.get_python_inc(True))').strip
@@ -57,20 +59,20 @@ class OpenBabel < Formula
 
   def caveats
     s = ''
-    if build.with?('python') and build.with?('python3')
-      s += <<-EOS.undent
+    if build.with?('python@2') and build.with?('python')
+      s += <<~EOS
 
         Open Babel cannot be installed from homebrew with simultaneous support
         for both Python 2 and 3. Only Python 3 support has been provided.
       EOS
     end
-    s = <<-EOS.undent
+    s = <<~EOS
       Instead of using the --with-python option, you may wish to install the
       python language bindings independently using pip:
         pip install openbabel
     EOS
     if build.with?('java')
-      s += <<-EOS.undent
+      s += <<~EOS
 
         Java libraries are installed to:
           #{HOMEBREW_PREFIX}/lib
