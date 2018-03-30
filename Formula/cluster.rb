f9190590248c6e8df426270b36d5d3dc8f306cb5
Matt Swain <m.swain@me.com>
2018-03-30 21:24:06 +0100

    cluster: Update to v1.54

--- Formula/cluster.rb
+++ Formula/cluster.rb
@@ -1,7 +1,7 @@
 class Cluster < Formula
   homepage "http://bonsai.hgc.jp/~mdehoon/software/cluster/software.htm"
-  url "http://bonsai.hgc.jp/~mdehoon/software/cluster/cluster-1.52a.tar.gz"
-  sha256 "e503a1b4680341a516b28804b3bfdbdefd7d8f01a0db659da7cb23f708ac1cd1"
+  url "http://bonsai.hgc.jp/~mdehoon/software/cluster/cluster-1.54.tar.gz"
+  sha256 "0b0ca0b79644de21bd0f7071eeb947ed2d7f21bda1402b07567deeff17f3e6fe"
 
   option 'with-lesstif',  'Build GUI'
 
@@ -15,6 +15,6 @@ class Cluster < Formula
   end
 
   test do
-    system "cluster", "-h"
+    system "#{bin}/cluster", "-h"
   end
 end
